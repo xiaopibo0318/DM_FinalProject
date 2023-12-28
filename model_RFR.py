dd274fe834006e569a5a14f86acc911b2e91cf7d
xiaopibo0318 <bobo661130@gmail.com>
2023-12-28 22:53:37 +0800

    Update

--- model_RFR.py
+++ model_RFR.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score 
-from sklearn.ensemble import RandomForestRegressor as RFC
+from sklearn.ensemble import RandomForestRegressor as RFR
 from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split
 #梯度提升樹、決策樹、RFC
 #XGBOOST 弱評估booster
@@ -14,18 +14,18 @@ y = data['Global_Sales']
 x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=90)
 
 #初始隨機森林回歸
-rfc = RFC(n_estimators=100,random_state=90,min_samples_leaf=1,max_depth=7)
-#rfc = RFC(n_estimators=100,random_state=90)
-rfc.fit(x_train,y_train)
-prediction = rfc.predict(x_test)
+rfr = RFR(n_estimators=100,random_state=90,min_samples_leaf=1,max_depth=7)
+#rfc = RFR(n_estimators=100,random_state=90)
+rfr.fit(x_train,y_train)
+prediction = rfr.predict(x_test)
 #print(prediction)
-score_pre = cross_val_score(rfc,x_train,y_train,cv=10).mean()
+score_pre = cross_val_score(rfr,x_train,y_train,cv=10).mean()
 print(f"平方交叉score: {score_pre}")
 # print(f"train r2:{r2_score(y_train,rfc.predict(x_train))}")
 # print(f"test  r2:{r2_score(y_test,prediction)}")
 
 # #調參 ,"max_depth":range(5,100)
-# rfc=RFC(n_estimators=100,random_state=90)
+# rfr=RFR(n_estimators=100,random_state=90)
 # param = {"min_samples_leaf": range(1,20)}     #要調整的參數
 # gs = GridSearchCV(estimator=rfc,param_grid=param,cv=5)
 # gs.fit(x_train,y_train)
@@ -39,6 +39,6 @@ print(f"平方交叉score: {score_pre}")
 
 answerData = pd.read_csv("newPredictGlobal.csv")
 #answerData = answerData.drop(columns=['Unnamed: 10'])
-myPredict = rfc.predict(answerData)
+myPredict = rfr.predict(answerData)
 answerData["Global_Sales"] = myPredict
 answerData.to_csv("40871223H_answer.csv",index=False)
--- model_XGBOOST.py
+++ model_XGBOOST.py
@@ -4,24 +4,16 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import cross_val_score, train_test_split
 
-
-
-
-#梯度提升樹、決策樹、RFC
-#XGBOOST 弱評估booster
 file_path = 'originalData.csv'
 data = pd.read_csv(file_path)
 
 X = data.drop(['Global_Sales'], axis=1)
 y = data['Global_Sales']
-
 x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=90)
 
-# Training the XGBoost model
 model = XGBRegressor(objective ='reg:squarederror')
 model.fit(x_train, y_train)
 
-# Evaluating the model
 y_pred = model.predict(x_test)
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
